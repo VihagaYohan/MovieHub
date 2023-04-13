f1841eeda63cdbc79061959220ae26517aa33196
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-04-13 20:08:12 +0800

    Updated constants

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import {Dimensions} from 'react-native'
 
+// constants
+import {DIMENSIONS} from '.'
+
 const PADDING = 10
+const DEFAULT_WIDTH = DIMENSIONS.screenWidth - (PADDING*2)
+const RADIUS = 10
 
 export default {
-    PADDING
+    PADDING,
+    DEFAULT_WIDTH,
+    RADIUS
 }
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,36 +1,50 @@
-import React, { Component,useState,useEffect,useCallback,useMemo,useLayoutEffect } from 'react'
-import {StyleSheet,View,Text,ViewStyle} from 'react-native'
-import {NativeStackNavigationProp} from '@react-navigation/native-stack'
+import React, {
+  Component,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useLayoutEffect,
+} from 'react';
+import {StyleSheet, View, Text, ViewStyle} from 'react-native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
 
 // components
-import {UIContainer} from '../components'
+import {UIContainer, UIRating} from '../components';
 
 // constants
+import {COLORS, CONSTANTS, STYLES} from '../constants';
 
 const HomeScreen = ({
-    navigation
-}:{
-    navigation:NativeStackNavigationProp<any,any>
-})=>{
-    
-    // navigation properties
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-            headerTitle:"Top Rated Movies",
-        })
-    })
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<any, any>;
+}) => {
+  // navigation properties
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Top Rated Movies',
+    });
+  });
 
-    return(
-       <UIContainer parentContainer={styles.container}>
-            <Text>Home screen</Text>
-       </UIContainer>
-    )
-}
+  return (
+    <UIContainer>
+      <View style={[styles.itemContainer, STYLES.shadow]}></View>
+
+      <UIRating />
+
+      
+    </UIContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        
-    }
-})
+  itemContainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'white',
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
